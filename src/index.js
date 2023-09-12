8cd61ade0d36fd126016422eacea6387aff4206e
Vu <longvunguyen2003@gmail.com>
2023-09-12 17:56:59 +0700

    MAKE A FOLDER FOR CORE MODULES

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import App from "./app-module";
+import App from './core-components/app-module';
 
+//FLOW OF THE App
 //create a new project
 App.addProject("New Project");
 let project = App.getProject(0);
@@ -10,7 +11,9 @@ let todoInfo = {
     discription: "display controller for displaying the content of the app",
     time: "09/09/2023 9AM",
     priority: 1,
-}
+} 
+//get user input from display controller
 project.addTodo(todoInfo);
 
 console.log(project.getTodo(0).getInfo());
+console.log(App.showApp());
